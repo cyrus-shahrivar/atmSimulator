ee47a19320077afb760f354447678b09e5051a3f
Cyrus Shahrivar <cyrus.shahrivar@gmail.com>
2017-08-13 20:08:21 -0400

    add congrats and welcome image; cache variables; add balance mod actions to deposit and withdrawl

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,4 +1,58 @@
+// TODOs Punchlist
+/*
+After prototype user testing:
+- error handling
+- fancy design
+*/
+
+// Program Variables
+///////////////////////////////////////////////////////////
+// NOTE: each click event has a different function depending on the state of the program
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+var stateContainer = {
+    state: 'login'
+    // states:
+    // 'login'
+    // 'show balance'
+    // 'withdrawl'
+    // 'withdrawl confirm'
+    // 'deposit'
+    // 'deposit confirm'
+    // 'finish'
+    // 'error'
+};
+
+var hasAssistanceMode = false;
+// Buttons
+var $modalCloseButton = $('#dismissal-x');
+var $keypadNumbers = $('.keypad button.number');
+var $keypadEnter = $('.keypad button.enter');
+var $showBalanceButton = $('#show-balance');
+var $withdrawlButton = $('#withdrawl');
+var $depositButton = $('#deposit');
+var $finishButton = $('#exit-or-finish');
+var $assistanceButton = $('.assistanceButton');
+// Slots
+var $cardSlot = $('section.card');
+var $depositSlot = $('section.deposit');
+var $withdrawlSlot = $('section.withdrawl');
+var $receiptSlot = $('section.receipt');
+// Other Elements
+var $modal = $('#modal');
+var $screenMessage = $('#screen .prompt-message');
+var $screenDirections = $('.screen-directions');
+var $pinEntry = $('.pin-entry');
+var $pinAsterisks = $('.pin-entry .row-asterisks');
+var $numberEntryContainer = $('.number-entry');
+var $numberEntry = $('.number-entry .row-numbers');
+var $keypadContainer = $('section.keypad-container');
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+
 // Business Logic - Init functions
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 function createAPin () {
     var start = 1000;
     var endNotInclusive = 10000;
@@ -17,8 +71,12 @@ function saveTestPinToLocalStorage(){
 function createAccount() {
     localStorage.setItem('balance', Math.floor(Math.random()*1000));
 }
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 
 // Business Logic - Util functions
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 function getPinFromLocalStorage() {
     return localStorage.getItem('pin');
 }
@@ -57,207 +115,181 @@ function withdrawl(amount) {
         return false;
     }
 }
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 
 // UI Utils
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 function displayPin(pin) {
     var $pin = $('#pin');
     $pin.html('Your PIN is ' + pin);
     $pin.fadeIn();
 }
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 
-// General Flow
-/*
-1. User is presented with welcome splash screen and PIN
-2. Instruction dialogue appears on atm screen
-3. Program listens for inputs from buttons and slots.
-4. Program modifies dialogue on screen as user advances.
-END: User is presented with congratulations screen
-*/
+// Initial Click Handlers
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+$modalCloseButton.click(function(){
+    if (stateContainer.state === 'login') {
+        $modal.fadeOut('fast');
+        $('main').css({
+            display: 'flex'
+        });
+        $screenMessage.text('Welcome to The Bank\'s ATM! Please insert your debit card to continue.');
+        if (hasAssistanceMode) {
+            $cardSlot.addClass('glow-yellow');
+        }
+    }
+});
+
+$cardSlot.click(function () {
+    if (stateContainer.state === 'login') {
+        $screenMessage.text('Please enter your pin.');
+        if (hasAssistanceMode) {
+            $cardSlot.removeClass('glow-yellow');
+            $keypadContainer.addClass('glow-yellow');
+        }
+        $pinEntry.show();
+    } else if (stateContainer.state === 'finish') {
+        // state === 'finish'
+        $screenMessage.text('Thank you for using The Bank\'s ATM! Please take your card and recepit. Have a nice day!');
+    }
+})
+
+$keypadNumbers.click(function(e){
+    if (stateContainer.state === 'login') {
+        if ($pinAsterisks.text().length < 4) {
+            var currentPin = localStorage.getItem('testPin')
+            localStorage.setItem('testPin', currentPin + e.target.innerHTML)
+            $pinAsterisks.append('<span>*</span>');
+        }
+        if (hasAssistanceMode) {
+            $keypadContainer.removeClass('glow-yellow');
+        }
+    } else if (stateContainer.state === 'deposit' || stateContainer.state === 'withdrawl') {
+        // show what is being typed in similar to pin and save amount as edited
+        console.log(e.target.innerHTML)
+        $numberEntryContainer.show();
+        $numberEntry.append('<span>' + e.target.innerHTML + '</span>');
+    }
+});
+
+$keypadEnter.click(function(e){
+    if (stateContainer.state === 'login') {
+        var testPin = getTestPinFromLocalStorage();
+        if (loginToAccount(testPin) === 'login') {
+            $screenMessage.text('Which action would you like to take?');
+            $pinEntry.hide();
+            $screenDirections.css({
+                display: 'flex'
+            });
+        } else {
+            saveTestPinToLocalStorage('')
+            $pinAsterisks.html('');
+            $screenMessage.text('Please reenter you pin.')
+        }
+    } else if (stateContainer.state === 'deposit') {
+        stateContainer.state = 'deposit confirm';
+        $screenMessage.text('Are you sure you would like to deposit this amount?');
+    } else if (stateContainer.state === 'deposit confirm') {
+        $numberEntryContainer.hide();
+        $screenMessage.text('Please deposit money into deposit slot below');
+    } else if (stateContainer.state === 'withdrawl') {
+        stateContainer.state = 'withdrawl confirm';
+        $screenMessage.text('Are you sure you would like to withdrawl this amount?');
+    } else if (stateContainer.state === 'withdrawl confirm') {
+        $numberEntryContainer.hide();
+        $screenMessage.text('Please take your money from the withdrawl slot');
+    }
+});
+
+$depositSlot.click(function () {
+    if (stateContainer.state === 'deposit confirm') {
+        var amountToDeposit = $numberEntry.text().split('$')[1];
+        deposit(amountToDeposit);
+        $numberEntry.text('$');
+        $screenMessage.html('');
+        $screenMessage.append('<p>Cash deposited.</p>');
+        $screenMessage.append('<p>Your new balance is: $' + getBalance() + ' </p>');
+        $screenMessage.append('<p>Which action would you like to take?</p>');
+    }
+})
+
+$withdrawlSlot.click(function () {
+    if (stateContainer.state === 'withdrawl confirm') {
+        var amountToWithdrawl = $numberEntry.text().split('$')[1];
+        withdrawl(amountToWithdrawl);
+        $numberEntry.text('$');
+        $screenMessage.html('');
+        $screenMessage.append('<p>Cash withdrawn.</p>');
+        $screenMessage.append('<p>Your new balance is: $' + getBalance() + ' </p>');
+        $screenMessage.append('<p>Which action would you like to take?</p>');
+    }
+})
+
+$receiptSlot.click(function () {
+    $modal.find('h1').text('Congratulations!');
+    $modal.find('h2').text('You have completed trying out the ATM simulator.');
+    $modalCloseButton.hide();
+    $modal.fadeIn();
+})
+
+$showBalanceButton.click(function(){
+    if ((stateContainer.state !== 'deposit' || stateContainer.state !== 'withdrawl') && stateContainer.state !== 'finish') {
+        stateContainer.state = 'show balance';
+    }
+    if (stateContainer.state === 'show balance') {
+        var balance = getBalance();
+        $screenMessage.text('Your balance is $' + balance + '.  \n Which action would you like to take?');
+    }
+});
+
+$withdrawlButton.click(function(){
+    if (stateContainer.state !== 'deposit' && stateContainer.state !== 'finish') {
+        stateContainer.state = 'withdrawl';
+    }
+    if (stateContainer.state === 'withdrawl') {
+        $screenMessage.text('How much would you like to withdrawl?');
+    }
+});
+
+$depositButton.click(function(){
+    if (stateContainer.state !== 'withdrawl' && stateContainer.state !== 'finish') {
+        stateContainer.state = 'deposit';
+    }
+    if (stateContainer.state === 'deposit') {
+        $screenMessage.text('How much would you like to deposit?');
+    }
+});
+
+$finishButton.click(function(){
+    stateContainer.state = 'finish';
+    $screenMessage.html('');
+    $screenMessage.append('<p>Returning card and printing receipt.</p>')
+    $screenMessage.append('<p>Thank you for using The Bank\'s ATM! Please take your card and recepit. Have a nice day!</p>');
+});
+
+$assistanceButton.click(function () {
+    hasAssistanceMode = !(hasAssistanceMode);
+    var message = hasAssistanceMode ? 'On' : 'Off';
+    $(this).html('Help Mode ' + message);
+});
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 
 // Init Flow
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 createAccount();
 savePinToLocalStorage(createAPin());
 saveTestPinToLocalStorage();
 displayPin(getPinFromLocalStorage());
 
 (function modal(){
-    $('#modal').fadeIn('slow');
+    $modal.fadeIn('fast');
 })();
-
-// NOTE: each click event has a different function depending on the state of the program
-
-var stateContainer = {
-    state: 'login'
-    // states:
-    // 'login'
-    // 'show balance'
-    // 'withdrawl'
-    // 'deposit'
-    // 'deposit confirm'
-    // 'finish'
-    // 'error'
-}
-
-// Initial Click Handlers
-$('#dismissal-x').click(function(){
-    if (stateContainer.state === 'login') {
-        $('#modal').fadeOut('fast');
-        $('#screen .prompt-message').text('Welcome to The Bank\'s ATM! Please insert your debit card to continue.');
-        $('.card').addClass('glow-yellow');
-    }
-});
-
-$('section.card').click(function () {
-    if (stateContainer.state === 'login') {
-        $('#screen .prompt-message').text('Please enter your pin.');
-        $('.card').removeClass('glow-yellow');
-        $('section.keypad-container').addClass('glow-yellow');
-        $('.pin-entry').show();
-    } else if (stateContainer.state === 'finish') {
-        // state === 'finish'
-        $('#screen .prompt-message').text('Thank you for using The Bank\'s ATM! Please take your card and recepit. Have a nice day!');
-    }
-})
-
-$('.keypad button.number').click(function(e){
-    if (stateContainer.state === 'login') {
-        if ($('.pin-entry .row-asterisks').text().length < 4) {
-            var currentPin = localStorage.getItem('testPin')
-            localStorage.setItem('testPin', currentPin + e.target.innerHTML)
-            $('.pin-entry .row-asterisks').append('<span>*</span>');
-        }
-        $('section.keypad-container').removeClass('glow-yellow');
-    } else if (stateContainer.state === 'deposit' || stateContainer.state === 'withdrawl') {
-        // show what is being typed in similar to pin and save amount as edited
-        console.log(e.target.innerHTML)
-        $('.number-entry').show();
-        $('.number-entry .row-numbers').append('<span>' + e.target.innerHTML + '</span>');
-    }
-});
-
-$('.keypad button.enter').click(function(e){
-    if (stateContainer.state === 'login') {
-        var testPin = getTestPinFromLocalStorage();
-        if (loginToAccount(testPin) === 'login') {
-            $('#screen .prompt-message').text('Which action would you like to take?');
-            $('.pin-entry').hide();
-            $('.screen-directions').css({
-                display: 'flex'
-            });
-        } else {
-            saveTestPinToLocalStorage('')
-            $('.pin-entry .row-asterisks').html('');
-            $('#screen .prompt-message').text('Please reenter you pin.')
-        }
-    } else if (stateContainer.state === 'deposit') {
-        stateContainer.state = 'deposit confirm';
-        $('#screen .prompt-message').text('Are you sure you would like to deposit this amount?');
-    } else if (stateContainer.state === 'deposit confirm') {
-        $('.number-entry').hide();
-        $('#screen .prompt-message').text('Please deposit money into deposit slot below');
-    } else if (stateContainer.state === 'withdrawl') {
-        stateContainer.state = 'withdrawl confirm';
-        $('#screen .prompt-message').text('Are you sure you would like to withdrawl this amount?');
-    } else if (stateContainer.state === 'withdrawl confirm') {
-        $('.number-entry').hide();
-        $('#screen .prompt-message').text('Please take your money from the withdrawl slot');
-    }
-});
-
-$('section.deposit').click(function () {
-    if (stateContainer.state === 'deposit confirm') {
-        $('#screen .prompt-message').html('');
-        $('#screen .prompt-message').append('<p>Cash deposited.</p>');
-        $('#screen .prompt-message').append('<p>Your new balance is: </p>');
-        $('#screen .prompt-message').append('<p>Which action would you like to take? [Show Balance, Withdrawl, Deposit, Exit]</p>');
-    }
-})
-
-$('section.withdrawl').click(function () {
-    if (stateContainer.state === 'withdrawl confirm') {
-        $('#screen .prompt-message').html('');
-        $('#screen .prompt-message').append('<p>Cash withdrawn.</p>');
-        $('#screen .prompt-message').append('<p>Your new balance is: </p>');
-        $('#screen .prompt-message').append('<p>Which action would you like to take? [Show Balance, Withdrawl, Deposit, Exit]</p>');
-    }
-})
-
-$('section.receipt').click(function () {
-    if (stateContainer.state === 'finish') {
-        $('#screen .prompt-message').text('Thank you for using The Bank\'s ATM! Please take your card and recepit. Have a nice day!');
-    }
-})
-
-$('#show-balance').click(function(){
-    if ((stateContainer.state !== 'deposit' || stateContainer.state !== 'withdrawl') && stateContainer.state !== 'finish') {
-        stateContainer.state = 'show balance';
-    }
-    if (stateContainer.state === 'show balance') {
-        var balance = getBalance();
-        $('#screen .prompt-message').text('Your balance is $' + balance + '.  \n Which action would you like to take?');
-    }
-});
-
-$('#withdrawl').click(function(){
-    if (stateContainer.state !== 'deposit' && stateContainer.state !== 'finish') {
-        stateContainer.state = 'withdrawl';
-    }
-    if (stateContainer.state === 'withdrawl') {
-        $('#screen .prompt-message').text('How much would you like to withdrawl?');
-    }
-});
-
-$('#deposit').click(function(){
-    if (stateContainer.state !== 'withdrawl' && stateContainer.state !== 'finish') {
-        stateContainer.state = 'deposit';
-    }
-    if (stateContainer.state === 'deposit') {
-        $('#screen .prompt-message').text('How much would you like to deposit?');
-    }
-});
-
-$('#exit-or-finish').click(function(){
-    stateContainer.state = 'finish';
-    $('#screen .prompt-message').text('Thank you for using The Bank\'s ATM! Please take your card and recepit. Have a nice day!');
-});
-
-// Dialogue
-/*
-1. Welcome to the ATM Simulator! Your PIN is ####
-2. Welcome to The Bank's ATM! Please insert your debit card to continue.
-3. Please enter your pin.
-[CORRECT PIN]
-4. Which action would you like to take? [Show Balance, Withdrawl, Deposit, Exit]
-[INCORRECT PIN] - Step 3.
-[Show Balance]
-1. Your balance is ####.
-2. Which action would you like to take? [Withdrawl, Deposit, Finish]
-[Withdrawl]
-1. How much would you like to withdrawl?
-2. Dispensing cash in withdrawl slot.
-3. Please remove cash from withdrawl slot.
-4. Which action would you like to take? [Withdrawl, Deposit, Finish]
-[Deposit]
-1. How much would you like to deposit?
-2. Please insert cash into deposit slot.
-3. Cash deposited.
-4. Which action would you like to take? [Withdrawl, Deposit, Finish]
-[Finish]
-1. Returning card and printing receipt.
-2. Thank you for using The Bank's ATM! Please take your card and receipt. Have a nice day!
-3. <Go to Step 2>
-[Exit] - option is always present
-1. Returning card and printing receipt.
-2. Thank you for using The Bank's ATM! Please take your card and recepit. Have a nice day!
-3. <Go to Step 2>
-*/
-
-
-// TODOs
-/*
-- error handling
-- calculate balance after action
-- congratulations message
-- toggle assistance mode
-- fancy design
-*/
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
